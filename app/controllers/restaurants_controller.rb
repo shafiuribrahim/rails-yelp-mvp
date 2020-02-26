e49fd17e4e2a844380e4fd44cee631e0315f83ee
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-02-26 13:11:55 +0800

    validated and created models

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
